4b24d2d416298f03fd48c27d561a1c2f4bce7786
keithhegit <keith@ogcloud.com>
2025-11-02 23:16:04 +0800

    Fix: 优化 Cloudflare Pages _headers 文件格式，修复 JSX MIME 类型问题

--- vite.config.js
+++ vite.config.js
@@ -8,19 +8,12 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: false
-      }
-    },
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom']
-        },
-        assetFileNames: 'assets/[name].[ext]'
+        }
       }
     }
   },
